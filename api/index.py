4bfdebbe702c9c2ed211bd01ea481322fc06b3ec
Asteroidvr101 <projectinfectedoffical@gmail.com>
2025-05-08 20:17:55 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -56,11 +56,11 @@ def playfab_authentication():
                     if len(banexpirelist) > 0
                     else "Infinite"
                 }
-
+                print(bannywanny)
                 return jsonify({
                     "message": banmessage,
                     "expire": banexpiretime,
-                })
+                }), 403
             else: 
                 errormessage = bannywanny.get("errorMessage", "Unknown error")
                 return jsonify({"Error": "PlayFab Error", "Message": errormessage}), 403
