2a33e2f631fcaf7efb7306e1339e0647552abdcd
Asteroidvr101 <projectinfectedoffical@gmail.com>
2025-05-09 01:34:50 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,6 +8,7 @@ class GameSettings:
     def __init__(self):
         self.TitleId: str = "7AF94"
         self.SecretKey: str = "GBIPB74594RF9UDYHIAKASEJ1WG66KWWF4FAPKJK1WYZCC94S7"
+        self.WebhookUrl: str = "https://discord.com/api/webhooks/1360361457820373206/GmmIFUI8NOQ9yQBovEfFfNWASr4CaF8UEBhRAHtBpw8rfQWS4qKbtPKpjGwESYbRsoqq"
 
     def auth_headers(self):
         return {
@@ -23,6 +24,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'I Think The Backend Is Working'
+
+pollers = [
+    {
+        "PollId": 3,
+        "Question": "Do You Like This Backend?",
+        "VoteOptions": ["YES", "NO"],
+        "VoteCount": [],
+        "PredictionCount": [],
+        "StartTime": "2025-05-09T00:00:00Z",
+        "EndTime": "2025-05-10T00:00:00Z",
+        "isActive": True
+    }
+]
 
 
 @app.route('/api/PlayFabAuthentication', methods=['POST', 'GET'])
@@ -76,6 +90,37 @@ def title_data():
     if requestdata.status_code == 200:
         return jsonify(requestdata.json())
 
+
+@app.route('/api/FetchPoll', methods=['GET', 'POST'])
+def fetch_poll():
+    return jsonify(pollers), 200
+
+
+@app.route('/api/Vote', methods=['GET', 'POST'])
+def vote():
+    data = request.get_json()
+    poll_id = data.get("PollId")
+    optionindex = data.get("OptionIndex")
+    isprediction = data.get("IsPrediction")
+    playfabid = data.get("PlayFabId")
+    sigma = next((poll for poll in pollers if poll["PollId"] == poll_id), None)
+
+    if not sigma:
+            return "Poll Not Found", 404
+    if optionindex < 0 or optionindex >= len(sigma["voteOptions"]):
+            return 'Invalid Vote Option', 400
+        
+
+    embed = {
+        "embeds": [
+            {
+                "title": "New Vote",
+                "description": f"Poll ID: {poll_id}\nOption: {sigma['VoteOptions'][optionindex]}\nIs Prediction: {isprediction}\nPlayFab ID: {playfabid}"
+            }
+        ]
+    }
+
+    requests.post(settings.WebhookUrl, json=embed)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
