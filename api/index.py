4032e792ef7a42d84db51328ce006ce454d97c3e
Asteroidvr101 <projectinfectedoffical@gmail.com>
2025-05-24 21:12:36 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -98,7 +98,7 @@ def playfab_authentication():
             ban_expiration = (
                 ban_expiration_list[0]
                 if len(ban_expiration_list) > 0
-                else "Idk"
+                else "Indefinite"
             )
 
             return jsonify({
