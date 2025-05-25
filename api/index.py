b01ec2e970a05fbf29e2c9f36d782e6a53e4a92b
Asteroidvr101 <projectinfectedoffical@gmail.com>
2025-05-24 20:44:51 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -89,21 +89,17 @@ def playfab_authentication():
             "EntityType": entity_type
         }), 200
     else: 
-        ban_info = login_req.json()
-        if ban_info.get("errorCode") == 1002:
-            ban_message = ban_info.get("errorMessage", "No ban message provided.")
-            ban_details = ban_info.get("errorDetails", {})
-            ban_expiration_key = next(iter(ban_details.keys()), None)
-            ban_expiration_list = ban_details.get(ban_expiration_key, [])
-            ban_expiration = (
-                ban_expiration_list[0]
-                if len(ban_expiration_list) > 0
-                else "Indefinite"
-            )
-
+        banshitty = requestlog.json()
+        if banshitty.get('errorCode') == 1002:
+            banmessage = banshitty.get('errorMessage', 'No Message Found')
+            bandetails = banshitty.get('errorDetails', {})
+            banexpkey = next(iter(bandetails.keys()), None)
+            banexplist = bandetails.get(banexpkey, [])
+            banexp = banexplist[0] if len(banexplist) > 0 else 'Infinite'
+            print (banshitty)
             return jsonify({
-                "BanMessage": ban_expiration_key,
-                "BanExpirationTime": ban_expiration,
+                "BanMessage": banexpkey,
+                "BanExpirationTime": banexp
             }), 403     
 
 @app.route("/api/TitleData", methods=["POST", "GET"])
